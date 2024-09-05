974a8fc9634f14475c118495f7786f6fb5ff795a
Sara Rotenski <162700489+sararotenski@users.noreply.github.com>
2024-09-05 11:05:28 -0300

    Add files via upload

---
+++ hotfest-main/hotfest.js
@@ -0,0 +1,16 @@
+
+// Função de ir para a página de login
+
+function login() {
+
+    window.location.href = "login.html"
+
+}
+
+//Função de ir para a página de cadastro
+
+function cadastro() {
+
+    window.location.href = "cadastro.html"
+
+}
